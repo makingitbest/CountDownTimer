01d9f74a7f05370bc444c0f5a296447121a7d8e0
wangqiao <makingitbest@163.com>
2017-03-01 09:32:30 +0800

    Initial Commit

---
+++ CountDownTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountDownTimer
+//
+//  Created by WangQiao on 17/3/1.
+//  Copyright © 2017年 WangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountDownTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountDownTimer
+//
+//  Created by WangQiao on 17/3/1.
+//  Copyright © 2017年 WangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
